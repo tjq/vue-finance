eed037de15ef0761362a5115ea64582d442b56a2
Tyler <tylercusack@gmail.com>
2017-10-30 15:10:02 -0400

    First commit

---
+++ src/components/Hello.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="hello">
+		<el-container>
+			<el-aside width="200px">
+				<h1> vue-finance </h1>
+				<el-input v-model="ticker"> </el-input>
+				<el-button :type="text" @click="getTickerJSON()">Load</el-button>
+			</el-aside>
+			
+			<el-main>
+				<h2>{{ticker}}</h2>
+			</el-main>
+			
+		</el-container>
+    
+		
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      ticker: ''
+    }
+  },
+    
+  methods: {
+		getTickerJSON(){
+			this.ticker = ""; // just a test
+		}
+		
+	}
+}
+
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+@import url("//unpkg.com/element-ui@1.0.0-rc.3/lib/theme-default/index.css");
+
+h1, h2 {
+  font-weight: normal;
+}
+
+a {
+  color: #35495E;
+}
+</style>
