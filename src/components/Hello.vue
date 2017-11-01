1c85e108aae160877d1578b3c59dc18d8138c7f7
Tyler <tylercusack@gmail.com>
2017-10-31 23:45:06 -0400

    Added alpha-vantage support & added MACD

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -24,7 +24,7 @@
 					<el-col :span=8> 
 						<el-card>
 							<p>Percent Change</p>
-							<span style="font-size:2em">{{((percentChange - 1)*100).toFixed(2)}}%</span>
+							<span class="tile-text" :style="(percentChange - 1) < 0 ? 'color:red' : 'color:green'">{{((percentChange - 1)*100).toFixed(2)}}%</span>
 						</el-card>
 					</el-col>
 					<el-col :span=8> 
@@ -41,6 +41,16 @@
 					<el-col :span=24>
 						<el-card v-loading="loading">
 							<highstock :options="options"></highstock>
+						</el-card>
+					</el-col>
+				</el-row>
+				
+				<br>
+				
+				<el-row :gutter=20>
+					<el-col :span=24>
+						<el-card v-loading="loading">
+							<highcharts :options="smaOptions"></highcharts>
 						</el-card>
 					</el-col>
 				</el-row>
@@ -68,12 +78,14 @@ export default {
     return {
       ticker: 'AAPL',
 			apiKey: 'WyAYWfaPWbL7iU49Rfo6',
+			alphaVantageKey: '3VSR22AHR48O8GY3',
 			loading: false,
-			highChartsData : [[]],
+			highChartsData : [],
+			smaData: [],
 			avgRating: 0,
 			options: {
 				rangeSelector: {
-            selected: 1
+            selected: 2
         },
 
         title: {
@@ -86,33 +98,79 @@ export default {
             tooltip: {
                 valueDecimals: 2
             }
-        }]}
+        }]},
+			
+			smaOptions : {
+				title: {
+					text: ''
+				},
+				
+				legend: {
+					enabled: false
+				},
+				
+				xAxis: {
+					type: 'datetime'
+				},
+				
+				series: [{
+					type: 'area',
+					data: []
+				}]
+			}
     }
   },
     
   methods: {
 		getTickerJSON(){
+			this.highChartsData = [];
+			
 			this.loading = true;
-			var url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + this.ticker + '/data.json?api_key=' + this.apiKey
+
 			var arUrl = 'https://www.quandl.com/api/v3/datasets/CBARH/' + this.ticker + '.json?api_key=' + this.apiKey
 			
+			var url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol='+ this.ticker + '&apikey=' + this.alphaVantageKey
+			
+			var smaUrl = 'https://www.alphavantage.co/query?function=MACD&symbol='+ this.ticker + '&interval=daily&series_type=open&apikey=' + this.alphaVantageKey;
 			
 			axios.get(url).then(response => {
-				this.highChartsData = response.data.dataset_data.data.map(function(d){
-					return [new Date(d[0]).getTime(), d[4]];
-				});
+				// console.log(response.data['Time Series (Daily)']) // debug
+				var arr = Object.values(response.data);
+				// console.log(arr[1])
+				var obj = response.data['Time Series (Daily)']
+				for (var key in obj) {
+				  // console.log(key);
+					// console.log(obj[key]['5. adjusted close'])
+					this.highChartsData.push([new Date(key).getTime(), parseFloat(obj[key]['5. adjusted close'])]);
+				}
+				
+				console.log(this.highChartsData)
 				
 				// for some reason this comes in backwards ?
 				// highcharts wants it sorted
-				this.options.series[0].data = this.highChartsData.reverse();
-				// this.options.series.data = this.highChartsData;
-				console.log(this.highChartsData)
-				console.log(this.options.series[0].data)
+			  this.options.series[0].data = this.highChartsData
 				this.loading = false;
 			})
 			
 			axios.get(arUrl).then(response => {
 				this.avgRating = response.data.dataset.data[0][2]
+			})
+			
+			axios.get(smaUrl).then(response => {
+				this.smaData = [];
+				console.log(response.data)
+				var arr = Object.values(response.data);
+				// console.log(arr[1])
+				var obj = response.data['Technical Analysis: MACD']
+				for (var key in obj) {
+				  // console.log(key);
+					// console.log(obj[key]['5. adjusted close'])
+					this.smaData.push([new Date(key).getTime(), parseFloat(obj[key]['MACD_Hist'])]);
+					
+				}
+				this.smaOptions.series[0].data = this.smaData
+				// console.log(this.smaData)
+				
 			})
 		}
 		
@@ -124,14 +182,14 @@ export default {
 				return 0;
 			}
 			var tmp = this.highChartsData
-			return tmp.slice(-1)[0][1]
+			return tmp[0][1]
 		},
 		percentChange(){
 			if(this.highChartsData.length < 2){
 				return 1;
 			}
 			var tmp = this.highChartsData
-			return tmp.slice(-1)[0][1]/tmp.slice(-2)[0][1]
+			return tmp[0][1]/tmp.slice(1)[0][1]
 		}
 	}
 }
@@ -146,7 +204,9 @@ export default {
 h1, h2 {
   font-weight: normal;
 }
-	
+.tile-text{
+	font-size: 2em;
+}
 .container {
   max-width: 980px;
   margin: 20px auto;
