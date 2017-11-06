f543da02f4b96a3c9ca8c9968a245bdc835e2f61
Tyler <tylercusack@gmail.com>
2017-11-06 15:34:29 -0500

    :sparkles: Added login via firebase & firebaseUI

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,25 @@
   <div id="app">
 			<header>
 				<el-container class="container" style="margin-top:0px">
-				<span style="font-weight:800;">tyler-apps</span>
+					<span style="font-weight:800;">tyler-apps</span>
+					<el-dropdown style="margin-left:80%" v-show="email==''">
+						<span class="el-dropdown-link" style="color: white">
+							Login<i class="el-icon-arrow-down el-icon--right"></i>
+						</span>
+						<el-dropdown-menu slot="dropdown">
+							<div id="firebaseui-auth-container"></div>
+						</el-dropdown-menu>
+					</el-dropdown>
+					<el-dropdown style="margin-left:65%" v-show="email!=''">
+						<span class="el-dropdown-link" style="color: white">
+							{{email}}<i class="el-icon-arrow-down el-icon--right"></i>
+						</span>
+						<el-dropdown-menu slot="dropdown">
+							<el-dropdown-item>
+								<el-button @click="firebaseLogout">Logout</el-button>
+							</el-dropdown-item>
+						</el-dropdown-menu>
+					</el-dropdown>
 				</el-container>
 			</header>
 			<main>
@@ -14,10 +32,66 @@
 </template>
 
 <script>
+
+import firebase from 'firebase';
+import firebaseui from 'firebaseui';
+	
+var uiConfig = {
+      signInSuccessUrl: '/',
+      signInOptions: [firebase.auth.GoogleAuthProvider.PROVIDER_ID]
+     };
+	
+var config = {
+    apiKey: "AIzaSyAqWSktSjJLbWQJcgDIi7fQ74cIK8HnanY",
+    authDomain: "stocks-9a3a7.firebaseapp.com",
+    databaseURL: "https://stocks-9a3a7.firebaseio.com",
+    projectId: "stocks-9a3a7",
+    storageBucket: "",
+    messagingSenderId: "1089302503318"
+  };
+ 
+firebase.initializeApp(config);
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
+ui.start('#firebaseui-auth-container', uiConfig);
+	
 export default {
-  name: 'app'
+  name: 'app',
+	
+	data: function(){
+		return {
+			uid: '',
+			email: ''
+	
+			}
+	},
+	
+	created: function(){
+		firebase.auth().onAuthStateChanged((user) => {
+              if(user) {
+                this.uid = user.uid;
+                this.email = user.email;
+              }
+     })
+	},
+	
+	methods: {
+		firebaseLogout(){
+			firebase.auth().signOut().then(function() {
+				
+				console.log('Signed Out');
+			}, function(error) {
+				console.error('Sign Out Error', error);
+			});
+			
+			this.email = '';
+			this.uid = '';
+		}
+	}
+	
+	
 }
 </script>
+
 
 <style>
 body {
--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Element from 'element-ui'
-	
+import firebase from 'firebase'
 import VueHighcharts from 'vue-highcharts';
 import Highcharts from 'highcharts';
 
